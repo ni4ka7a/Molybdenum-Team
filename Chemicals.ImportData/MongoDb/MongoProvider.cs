2312ed096c68594a9169043b5d0c666f8d55e1f6
ni4ka7a <n.atanasow@yahoo.com>
2015-10-17 14:09:24 +0300

    MongoProvider and MongoImporter classes added.

---
+++ Chemicals.ImportData/MongoDb/MongoProvider.cs
@@ -0,0 +1,30 @@
+﻿namespace Chemicals.ImportData.MongoDb
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using MongoDB.Driver;
+
+    public class MongoProvider
+    {
+        public MongoProvider(string connectionString, string databaseName)
+        {
+            this.ConnectionString = connectionString;
+            this.DatabaseName = databaseName;
+        }
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public IMongoDatabase GetDatabase()
+        {
+            var client = new MongoClient(this.ConnectionString);
+            var db = client.GetDatabase(this.DatabaseName);
+
+            return db;
+        }
+    }
+}
