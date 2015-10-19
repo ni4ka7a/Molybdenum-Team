fd022925ac11012edf77d30078c2847000c445a5
ni4ka7a <n.atanasow@yahoo.com>
2015-10-19 11:33:51 +0300

    Added reference to mongoDB in startup project.

--- Chemicals.ConsoleTesterApp/Startup.cs
+++ Chemicals.ConsoleTesterApp/Startup.cs
@@ -4,6 +4,8 @@
     using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
+    using System.Configuration;
 
     using Chemicals.Data.SQLServer;
     using Chemicals.Data.SQLServer.Migrations;
@@ -11,12 +13,13 @@
     using Chemicals.ExcelImporter;
     using Chemicals.ExcelImporter.Contracts;
     using Chemicals.Models;
+    using Chemicals.ImportData.MongoDb;
 
     public class Startup
     {
         public static void Main()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ChemicalsDbContext, Configuration>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ChemicalsDbContext, Data.SQLServer.Migrations.Configuration>());
 
             var db = new ChemicalsDbContext();
             //var manufacturer = new Manufacturer() { Name = "PSI", NumberOfFactories = 5, Address = "Aleksandar Malinov 102" };
@@ -24,32 +27,46 @@
             //db.Manufacturers.AddOrUpdate(manufacturer);
             // db.SaveChanges();
 
-            IZipExtractor zipExtractor = new ZipExtractor();
-            ExcelImporter<Sale> k = new ExcelImporter<Sale>(zipExtractor);
-            ICollection<Sale> sales = k.ImportModelsDataFromDirectory(@".\tests");
+            //IZipExtractor zipExtractor = new ZipExtractor();
+            //ExcelImporter<Sale> k = new ExcelImporter<Sale>(zipExtractor);
+            //ICollection<Sale> sales = k.ImportModelsDataFromDirectory(@".\tests");
 
-            ExcelExporter<Sale> l = new ExcelExporter<Sale>();
-            l.ExportDataModelsCollectionToExcelFile(@".\", "Test2", "Test3", sales);
+            //ExcelExporter<Sale> l = new ExcelExporter<Sale>();
+            //l.ExportDataModelsCollectionToExcelFile(@".\", "Test2", "Test3", sales);
 
-            ICollection<Sale> salesZip = k.ImportModelsDataFromZipFile(@".\test.zip");
+            //ICollection<Sale> salesZip = k.ImportModelsDataFromZipFile(@".\test.zip");
 
-            var i = 1;
-            foreach (Sale sale in sales)
-            {
-                db.Sales.Add(sale);
+            //var i = 1;
+            //foreach (Sale sale in sales)
+            //{
+            //    db.Sales.Add(sale);
 
-                if (i % 100 == 0)
-                {
-                    db.SaveChanges();
-                    db.Dispose();
-                    db = new ChemicalsDbContext();
-                    i = 0;
-                }
+            //    if (i % 100 == 0)
+            //    {
+            //        db.SaveChanges();
+            //        db.Dispose();
+            //        db = new ChemicalsDbContext();
+            //        i = 0;
+            //    }
 
-                i++;
-            }
+            //    i++;
+            //}
 
-            db.SaveChanges();
+            //db.SaveChanges();
+
+            //var mongoProvider = new MongoProvider(
+            //    System.Configuration.ConfigurationManager.ConnectionStrings["MolybdenumDb"].ConnectionString,
+            //    System.Configuration.ConfigurationManager.ConnectionStrings["MolybdenumDb"].Name);
+
+            //var mongoDatabase = mongoProvider.GetDatabase();
+
+            //var mongoImporter = new MongoImporter();
+            //var products = mongoImporter.GetAllProducts(mongoDatabase, "Products");
+
+            //foreach (var item in products)
+            //{
+            //    System.Console.WriteLine(item.Name);
+            //}
         }
     }
 }
