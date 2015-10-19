35893bcf9c06f7385840cf3a21c65dd075252400
GoranTelerik <gorancvetkov_sr@yahoo.com>
2015-10-19 15:30:29 +0300

    SQLite DB added for radioactive product.

---
+++ Chemicals.SQLightData/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Chemicals.SQLightData
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new RadioactivityDbContext())
+            {
+                RadioactiveProduct product= new RadioactiveProduct();
+                product.Radioactivity = 4;
+                product.ProductName = "djas";
+                db.RadioactiveProducts.Add(product);
+
+                db.SaveChanges();
+
+                foreach (var radioactiveProduct in db.RadioactiveProducts)
+                {
+                    Console.WriteLine(radioactiveProduct.ProductName);
+                }
+               // Console.WriteLine(db.RadioactiveProducts.Count());
+            }
+        }
+    }
+}
